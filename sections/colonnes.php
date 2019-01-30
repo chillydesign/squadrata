f96777df44419cd418cbeffe3e008bd858ba0475
Charles Harvey <harvey.charles@gmail.com>
2019-01-30 22:58:19 +0100

    fixed html in colonnes php

--- sections/colonnes.php
+++ sections/colonnes.php
@@ -1,12 +1,11 @@
-<?php $title = get_sub_field('title'); ?>
+
 
 <div class="container">
-    <?php if ($title): ?> <h2><span><?php echo $title; ?></span></h2> <?php endif; ?>
-	<div class="section_row">
+	<div class="columns">
 	<?php while ( have_rows('columns') ) : the_row(); ?>
-		<div class="section_col">
+		<div class="column">
 			<?php echo get_sub_field('content'); ?>
 		</div>
 	<?php endwhile; ?>
-	</div> <!-- END OF section_row -->
+</div> <!-- END OF columns -->
 </div><!--  END OF CONTAINER -->
