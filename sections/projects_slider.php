f3dac3f18cd55a13f1360db7198705ab82af3c2b
Charles Harvey <harvey.charles@gmail.com>
2019-03-27 17:17:09 +0100

    add projects slider section

---
+++ sections/projects_slider.php
@@ -0,0 +1,33 @@
+
+
+
+
+<div class="container">
+    <div id="projects_slider" data-slidestoshow="1">
+        <?php while ( have_rows('projects') ) : the_row(); ?>
+
+            <?php $projet =  get_sub_field('project'); ?>
+            <?php $proj_url = get_permalink($projet->ID); ?>
+            <?php $title =  get_sub_field('title'); ?>
+            <?php if (!$title || $title == '') $title = $projet->post_title; ?>
+            <?php $image = get_sub_field( 'image' ); ?>
+            <?php $description =  get_sub_field('description'); ?>
+
+            <div class="project project_full project_show_info" >
+
+                <?php if ($title): ?>
+                    <h2><a href="<?php echo $proj_url; ?>"><?php echo $title; ?></a></h2>
+                <?php endif; ?>
+                <?php if ($description): ?>
+                    <a  href="<?php echo $proj_url; ?>" class="project_info">
+                        <p><?php echo $description; ?></p>
+                    </a>
+                <?php endif; ?>
+                <?php if ($image): ?>
+                    <img src="<?php echo $image['sizes']['large'] ; ?>" />
+                <?php endif; ?>
+            </div>
+
+        <?php endwhile; ?>
+    </div><!-- END OF PROJECTS SLIDER -->
+</div>
