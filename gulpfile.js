d3c20dbd695f6e6d4990c918f47a6ccd430e6013
Charles Harvey <harvey.charles@gmail.com>
2019-06-10 13:27:52 +0100

    add submenu nav

--- gulpfile.js
+++ gulpfile.js
@@ -2,46 +2,38 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var minify = require('gulp-minify');
-//var browserSync = require('browser-sync');
+var watch = require('gulp-watch');
 
 
-gulp.task('heya', function() {
+gulp.task('heya', function(done) {
   console.log('I live! Gulp is alive!');
+      done();
 });
 
 
-gulp.task('sass', function(){
+
+
+
+gulp.task('sass', function(done){
   return gulp.src('scss/global.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(autoprefixer({
         cascade: false
     }))
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('css'));
+    done();
+
 });
 
-
-
-
-gulp.task('compress', function() {
-  gulp.src('js/*.js')
-    .pipe(minify({
-        ext:{
-            src:'-debug.js',
-            min:'.js'
-        },
-        exclude: ['tasks'],
-        ignoreFiles: ['.combo.js', '-min.js' , 'jquery.js' , '.min.js' ]
-    }))
-    .pipe(gulp.dest('js/min'))
+gulp.task('watch', function () {
+    watch('scss/*.scss',  gulp.series( 'sass' ));
 });
-
-
 
 
 
 // Run all Gulp tasks and serve application
-gulp.task('default', ['heya', 'sass'], function() {
-  gulp.watch('scss/**/*.scss', ['sass']);
-//  gulp.watch('js/**/*.js',  ['compress'] );
+gulp.task('default', gulp.series( 'sass', 'watch'), function() {
+    // sass function now incldes the watching of files
+
 });
