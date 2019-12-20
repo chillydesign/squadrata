5ed698209c052ce3701aff1de50cacc89ae92e50
Charles Harvey <harvey.charles@gmail.com>
2019-12-20 10:51:49 +0000

    remove desc from proj slider

--- sections/projects_slider.php
+++ sections/projects_slider.php
@@ -8,22 +8,22 @@
 
             <?php $projet =  get_sub_field('project'); ?>
             <?php $proj_url = get_permalink($projet->ID); ?>
-            <?php $title =  get_sub_field('title'); ?>
-            <?php if (!$title || $title == '') $title = $projet->post_title; ?>
+            <?php // $title =  get_sub_field('title'); ?>
+            <?php $title = $projet->post_title; ?>
             <?php $image = get_sub_field( 'image' ); ?>
-            <?php // $description =  get_sub_field('description'); ?>
+            
             <?php $subtitle = get_post_meta($projet->ID, 'subtitle', true); ?>
 
             <div class="project project_full project_show_info" >
 
-                <?php if ($title): ?>
-                    <h2><a href="<?php echo $proj_url; ?>"><?php echo $title; ?></a></h2>
-                <?php endif; ?>
-                <?php if ($subtitle): ?>
-                    <a  href="<?php echo $proj_url; ?>" class="project_info">
-                        <p><?php echo $subtitle; ?></p>
-                    </a>
-                <?php endif; ?>
+             
+                    <h2><a href="<?php echo $proj_url; ?>">
+                        <?php echo $title; ?>
+                        <?php if ($subtitle): ?>
+                            <span><?php echo $subtitle; ?></span>
+                        <?php endif; ?>
+                    </a></h2>
+                
                 <?php if ($image): ?>
                     <img src="<?php echo $image['sizes']['large'] ; ?>" />
                 <?php endif; ?>
